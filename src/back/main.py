37b4e069a260d178a7b247102d6824419d2d2140
Thomas Margueritat <thomas@marguerit.at>
2020-08-18 20:18:59 +0200

    added functions to get compuer informations with psutil

--- src/back/main.py
+++ src/back/main.py
@@ -1,10 +1,77 @@
 from fastapi import FastAPI
-import psutil
+import psutil, typing, json, time, datetime
 
 app = FastAPI()
 
-#for test
+def getCpuTimesPercents():
+    cpuTimesPercent = psutil.cpu_times_percent()
+    return json.dumps(
+        {
+            'userTimePercent': cpuTimesPercent[0],
+            'systemTimePercent': cpuTimesPercent[1],
+            'idleTimePercent': cpuTimesPercent[2]
+        }
+    )
+
+def getCpuNumber():
+    return json.dumps({'logical': psutil.cpu_count(), 'physical': psutil.cpu_count(logical=False)})
+
+def getCpuFrequence():
+    return round(psutil.cpu_freq()[0])
+
+def getMemory():
+    virtualMemory = psutil.virtual_memory()
+    swapMemory = psutil.swap_memory()
+    return json.dumps(
+        {
+            'virtualMemory': {
+                'total': virtualMemory[0],
+                'available': virtualMemory[1],
+                'used': virtualMemory[3],
+                'free': virtualMemory[4]
+            },
+            'swapMemory': {
+                'total': swapMemory[0],
+                'used': swapMemory[1],
+                'free': swapMemory[2]
+            }
+        }
+    )
+
+def getDiskUsage():
+    diskUsage = {}
+    for device in psutil.disk_partitions():
+        diskUsage[device[0]] = {
+            'total': psutil.disk_usage(device[0])[0],
+            'used': psutil.disk_usage(device[0])[1],
+            'free': psutil.disk_usage(device[0])[2]
+        }
+    return json.dumps(diskUsage)
+
+def getNetworkUsage():
+    networkUsage = psutil.net_io_counters()
+    return json.dumps(
+        {
+            'bytesSent': networkUsage[0],
+            'bytesReceive': networkUsage[1],
+            'packetsSent': networkUsage[2],
+            'packetsReceive': networkUsage[3] 
+        }
+    )
+
+def getBootTime():
+    return datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")
+
+def getUsers():
+    users = []
+    for user in psutil.users():
+        users.append(user[0])
+    return json.dumps(users)
+
+getCpuTimesPercents() #initilization
+
+time.sleep(1)
 
 @app.get("/")
 async def root():
-    return psutil.virtual_memory()
+    return "It works!"
